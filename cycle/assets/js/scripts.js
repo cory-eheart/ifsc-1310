e4dafedd4ae909ee9976cbe135dcfd75eec01253
cory-eheart <cleheart@ualr.edu>
2021-05-06 00:52:44 +0000

    Pushed JScript Demo

---
+++ cycle/assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+$('.gallery').cycle({
+		fx: 'turnLeft', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		speed: 1500,
+		pause: 1
+	});
+
+});
